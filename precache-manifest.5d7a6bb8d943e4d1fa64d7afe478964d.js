e15e80073aa0f816d13df1d6d4f368e6be8bc70e
uday755 <79998578+uday755@users.noreply.github.com>
2022-06-15 16:35:12 +0530

    Updates

---
+++ precache-manifest.5d7a6bb8d943e4d1fa64d7afe478964d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "accba93caf9abb3b2fd4",
+    "url": "/TextUtils/static/css/main.ea4a53fc.chunk.css"
+  },
+  {
+    "revision": "accba93caf9abb3b2fd4",
+    "url": "/TextUtils/static/js/main.ff51433c.chunk.js"
+  },
+  {
+    "revision": "0b62529671e3b413e5cb",
+    "url": "/TextUtils/static/js/runtime~main.3e3956a9.js"
+  },
+  {
+    "revision": "c6a7617e91aa296af569",
+    "url": "/TextUtils/static/js/2.3bc0b968.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "/TextUtils/static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "cc0e82ca9d31f9938a1be2eb04096973",
+    "url": "/TextUtils/index.html"
+  }
+];
